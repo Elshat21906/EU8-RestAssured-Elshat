3a4823286ab28b8228839e7397db20e1fbabeb9b
Elshat <z90602190@hotmail.com>
2022-08-04 06:33:47 +0300

    Day01:API Introduction

---
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day01/simpleGetRequest.java
@@ -0,0 +1,26 @@
+package com.cybertek.day01;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class simpleGetRequest {
+
+    String url = "http://44.202.63.224:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        // send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        // print response status code
+        System.out.println(response.statusCode());
+
+        // print the response body
+        response.prettyPrint();
+
+
+    }
+
+}
