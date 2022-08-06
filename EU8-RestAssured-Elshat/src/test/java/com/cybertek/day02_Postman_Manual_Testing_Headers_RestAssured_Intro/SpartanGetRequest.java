db5569fd2c10bb4234c9f6e65f5fdeb28ebfd9c1
Elshat <z90602190@hotmail.com>
2022-08-06 11:11:03 +0300

    Day02:Postman_Manual_Testing_Headers_RestAssured_Intro

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day02_Postman_Manual_Testing_Headers_RestAssured_Intro/SpartanGetRequest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day02_Postman_Manual_Testing_Headers_RestAssured_Intro/SpartanGetRequest.java
@@ -1,9 +1,7 @@
 package com.cybertek.day02_Postman_Manual_Testing_Headers_RestAssured_Intro;
 
 public class SpartanGetRequest {
-
     String url = "http://44.202.63.224:8000/api/spartans";
-
 
 //    Given Accept type application/json
 //    When user send GET request to api/spartans end point
