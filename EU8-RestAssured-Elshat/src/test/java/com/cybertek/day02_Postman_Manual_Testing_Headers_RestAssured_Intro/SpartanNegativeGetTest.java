f2fb0f70d7687dbfaab3a4e959248657f56cda79
Elshat <z90602190@hotmail.com>
2022-08-07 15:57:52 +0300

    Day02:Postman_Manual_Testing_Headers_RestAssured_Intro

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day02_Postman_Manual_Testing_Headers_RestAssured_Intro/SpartanNegativeGetTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day02_Postman_Manual_Testing_Headers_RestAssured_Intro/SpartanNegativeGetTest.java
@@ -1,7 +1,11 @@
 package com.cybertek.day02_Postman_Manual_Testing_Headers_RestAssured_Intro;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +15,32 @@ public class SpartanNegativeGetTest {
     @BeforeAll
     public static void init() {
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://44.202.63.224:1000/ords/hr";
+        baseURI = "http://44.202.63.224:8000";
+    }
+
+
+     /*TASK
+    Given Accept type application/xml
+    When user send GET request to /api/spartans/10 end point
+    Then status code must be 406
+    And response Content Type must be application/xml;charset=UTF-8
+    */
+
+    @DisplayName("Get request to /api/spartans/10")
+    @Test
+    public void test(){
+
+       Response response = given().accept(ContentType.XML)
+                .when()
+                .get(baseURI + "/api/spartans/10");
+
+        //verify status code is 406
+        assertEquals(406,response.statusCode());
+
+        //verify content type
+        assertEquals("application/xml;charset=UTF-8",response.contentType());
+
+
 
     }
 
