f4350d2a3e585c6b82377d4c926b8133d0ce7df3
Elshat <z90602190@hotmail.com>
2022-08-14 00:46:07 +0300

    Day04:JsonPath

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/CBTrainingApiWithJsonPath.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/CBTrainingApiWithJsonPath.java
@@ -1,7 +1,14 @@
 package com.cybertek.day04_JsonPath;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
 
 public class CBTrainingApiWithJsonPath {
 
@@ -12,6 +19,33 @@ public class CBTrainingApiWithJsonPath {
 
     }
 
+    @DisplayName("GET Request to individual student")
+    @Test
+    public void test1(){
+        //send a get request to student id 23401 as a path parameter and accept header application/json
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id",23401)
+                .when()
+                .get("api.cybertektraining.com/student/all");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        //verify status code=200 /content type=application/json;charset=UTF-8 /Content-Encoding = gzip
+
+        //verify Date header exists
+        //assert that
+
+            /*
+                firstName Vera
+                batch 14
+                section 12
+                emailAddress aaa@gmail.com
+                companyName Cybertek
+                state IL
+                zipCode 60606
+                using JsonPath
+             */
 
 
+    }
 }
