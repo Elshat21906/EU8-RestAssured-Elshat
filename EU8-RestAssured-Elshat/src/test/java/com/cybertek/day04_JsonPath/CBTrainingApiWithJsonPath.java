9bed9a05e19081b1a3776fae39366517e5dcc443
Elshat <z90602190@hotmail.com>
2022-08-13 23:38:54 +0300

    Day04:JsonPath

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/CBTrainingApiWithJsonPath.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/CBTrainingApiWithJsonPath.java
@@ -1,4 +1,17 @@
 package com.cybertek.day04_JsonPath;
 
+import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.baseURI;
+
 public class CBTrainingApiWithJsonPath {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://api.cybertektraining.com";
+
+    }
+
+
+
 }
--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/ORDSApiWithJsonPath.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/ORDSApiWithJsonPath.java
@@ -1,9 +1,12 @@
 package com.cybertek.day04_JsonPath;
 
 import com.cybertek.utilitis.HRTestBase;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,12 +21,20 @@ public class ORDSApiWithJsonPath  extends HRTestBase {
 
         //get the second country name with JsonPath
         //to use jsonpath we assign response to JsonPath
+        JsonPath jsonPath = response.jsonPath();
+        //response.prettyPrint();
 
+        String secondCountryName = response.jsonPath().getString("items[1].country_name");
+        System.out.println("secondCountryName = " + secondCountryName);
 
         //get all country ids
         //items.country_id
+        List<String> allCountryIDs = response.jsonPath().getList("items.country_id");
+        System.out.println("allCountryIDs = " + allCountryIDs);
 
         //get all country names where their region id is equal to 2
+        List<String> allCountryNamesWithRegionId2 = response.jsonPath().getList("items.findAll{it.region_id==2}.country_name");
+        System.out.println("allCountryNamesWithRegionId2 = " + allCountryNamesWithRegionId2);
 
     }
 
