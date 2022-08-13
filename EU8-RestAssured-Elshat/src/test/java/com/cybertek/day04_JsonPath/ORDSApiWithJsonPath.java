905bb72e931625ffb13deb87f3392f1a594583b1
Elshat <z90602190@hotmail.com>
2022-08-14 00:15:42 +0300

    Day04:JsonPath

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/ORDSApiWithJsonPath.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day04_JsonPath/ORDSApiWithJsonPath.java
@@ -38,4 +38,32 @@ public class ORDSApiWithJsonPath  extends HRTestBase {
 
     }
 
+    @DisplayName("GET request to /employees with query param")
+    @Test
+    public void test2(){
+
+        //we added limit query param to get 107 employees
+        Response response = given().queryParam("limit",107)
+                .when().get("/employees");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        //get me all email of employees who is working as IT_PROG
+        List<String> allEmailsOfEmplyees = response.jsonPath().getList("items.findAll{it.job_id==\"IT_PROG\"}.email");
+        System.out.println("allEmailsOfEmplyees = " + allEmailsOfEmplyees);
+
+        //get me first name of employees who is making more than 10000
+        List<String> empName = response.jsonPath().getList("items.findAll{it.salary > 10000}.first_name");
+        System.out.println("empName = " + empName);
+
+        //get the max salary first_name
+        String firstNameMaxSalary = jsonPath.getString("items.max{it.salary}.first_name");
+        System.out.println("firstNameMaxSalary = " + firstNameMaxSalary);
+
+        String firstNameMaxSalaryWithPathMethod = response.path("items.max{it.salary}.first_name");
+        System.out.println("firstNameMaxSalaryWithPathMethod = " + firstNameMaxSalaryWithPathMethod);
+
+
+    }
+
 }
