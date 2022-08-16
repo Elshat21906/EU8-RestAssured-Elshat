370796227066ca51dd64479f9471253ef0325b3e
Elshat <z90602190@hotmail.com>
2022-08-16 22:47:51 +0300

    Day05:Hamcrest Matchers,JSON to Java, Spartan DB vs API

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/ORDSHamcrestTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/ORDSHamcrestTest.java
@@ -2,6 +2,7 @@ package com.cybertek.day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API;
 
 import com.cybertek.utilitis.HRTestBase;
 import io.restassured.http.ContentType;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STLongHexNumber;
@@ -22,16 +23,16 @@ public class ORDSHamcrestTest extends HRTestBase {
 
     public void employeesTest(){
 
-        //send a get request to employees endpoint with query parameter job_id IT_PROG
+        //send a get request to employees end point with query parameter job_id IT_PROG
         //verify each job_id is IT_PROG
         //verify first names are .... (find proper method to check list against list)
         //verify emails without checking order (provide emails in different order,just make sure it has same emails)
         //expected names
 
-        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
 
+        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
         given().accept(ContentType.JSON)
-                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .and().queryParam("q","{\"job_id\":\"IT_PROG\"}")
                 .when()
                 .get("/employees")
         .then()
@@ -41,5 +42,6 @@ public class ORDSHamcrestTest extends HRTestBase {
                 .body("items.first_name",containsInRelativeOrder("Alexander","Bruce","David","Valli","Diana"))
                 .body("items.email",containsInAnyOrder("AHUNOLD","DLORENTZ","VPATABAL","DAUSTIN","BERNST"))
                 .body("items.first_name",equalTo(names));
+
     }
 }
