9f2745a5063f3fec3a84520330c3a41a9d8663b3
Elshat <z90602190@hotmail.com>
2022-08-20 15:06:36 +0300

    Day05:Hamcrest Matchers,JSON to Java, Spartan DB vs API

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/ORDSHamcrestTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/ORDSHamcrestTest.java
@@ -2,6 +2,8 @@ package com.cybertek.day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API;
 
 import com.cybertek.utilitis.HRTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.apache.poi.ss.formula.functions.T;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -49,11 +51,24 @@ public class ORDSHamcrestTest extends HRTestBase {
     public void test2(){
 
         //we want to chain and also get response object
+        JsonPath jsonPath = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"job_id\":\"IT_PROG\"}")
+                .when()
+                .get("/employees")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .body("items.job_id", everyItem(equalTo("IT_PROG")))
+                .extract().jsonPath();
+
+
 
         //extract() --> method that allow us to get response object after we use then() method.
         //assert that we have only 5 firstnames
+        assertThat(jsonPath.getList("items.first_name"),hasSize(5));
 
         //assert firstnames order
+        assertThat(jsonPath.getList("items.first_name"),containsInRelativeOrder("Alexander","Bruce","David","Valli","Diana"));
 
     }
 }
--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
@@ -1,7 +1,4 @@
 package com.cybertek.day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API;
 
-public class SpartanHamcrestTest {
-
-
-
+public class SpartanAPIvsDB {
 }
