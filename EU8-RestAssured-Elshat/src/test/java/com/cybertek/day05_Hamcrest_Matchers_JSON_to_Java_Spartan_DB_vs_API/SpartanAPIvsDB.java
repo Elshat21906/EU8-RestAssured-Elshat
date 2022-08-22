d76b9117ff72f00320ff89d5ba8e57b6855f2b4c
Elshat <z90602190@hotmail.com>
2022-08-22 20:47:32 +0300

    Day05:Hamcrest Matchers,JSON to Java, Spartan DB vs API

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
@@ -18,7 +18,7 @@ public class SpartanAPIvsDB extends SpartansTestBase {
 
     @DisplayName("Get one spartan from api and database")
     @Test
-    public void testDB1(){
+    public void testDB1() {
 
         //get id,name,gender phone  from database
         //get same information from api
@@ -45,34 +45,42 @@ public class SpartanAPIvsDB extends SpartansTestBase {
 
 
         //Deserialization here JSon to Java  with Jackson objectMapper
-        Map<String,Object> apiMap = response.as(Map.class);
+        Map<String, Object> apiMap = response.as(Map.class);
         System.out.println(apiMap);
 
         //3.compare database vs api --> we assume expected result is db
-        assertThat(apiMap.get("id").toString(),is(dbMap.get("SPARTAN_ID").toString()));
-        assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
-        assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
-        assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
+        assertThat(apiMap.get("id").toString(), is(dbMap.get("SPARTAN_ID").toString()));
+        assertThat(apiMap.get("name"), is(dbMap.get("NAME")));
+        assertThat(apiMap.get("gender"), is(dbMap.get("GENDER")));
+        assertThat(apiMap.get("phone").toString(), is(dbMap.get("PHONE").toString()));
 
     }
 
+
+        //get id,name,gender phone  from database
+        //get same information from api
+        //compare
+
+        //1. get id,name,gender phone  from database
+
+
+        //save data inside the map
+
+
+        //2.get info from api
+
+
+        //Deserialization here JSon to Java  with Jackson objectMapper
+
+
+        //3.compare database vs api --> we assume expected result is db
+
 }
-//get id,name,gender phone  from database
-//get same information from api
-//compare
-
-//save data inside the map
 
 
 
-//1. get id,name,gender phone  from database
 
 
-//2.get info from api
 
-
-//Deserialization here JSon to Java  with Jackson objectMapper
-
-//3.compare database vs api --> we assume expected result is db
 
 
