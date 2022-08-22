d8a2e34ef121cd399137d5b39a107064fc7d2a75
Elshat <z90602190@hotmail.com>
2022-08-22 13:08:04 +0300

    Day05:Hamcrest Matchers,JSON to Java, Spartan DB vs API

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanAPIvsDB.java
@@ -1,4 +1,34 @@
 package com.cybertek.day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API;
 
-public class SpartanAPIvsDB {
+import com.cybertek.utilitis.SpartansTestBase;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanAPIvsDB extends SpartansTestBase {
+
+    public void testDB1() {
+
+        //get id,name,gender phone  from database
+        //get same information from api
+        //compare
+
+
+
+        //1. get id,name,gender phone  from database
+
+        //save data inside the map
+
+        //2.get info from api
+
+
+        //Deserialization here JSon to Java  with Jackson objectMapper
+
+        //3.compare database vs api --> we assume expected result is db
+
+    }
+
 }
--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanHamcrestTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API/SpartanHamcrestTest.java
@@ -1,7 +1,53 @@
 package com.cybertek.day05_Hamcrest_Matchers_JSON_to_Java_Spartan_DB_vs_API;
 
-public class SpartanHamcrestTest {
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.cybertek.utilitis.SpartansTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class SpartanHamcrestTest extends SpartansTestBase {
+
+    @DisplayName("Get spartan/search and chaining together")
+    @Test
+    public void test1() {
+
+        //along with this statement, I want to save names inside the List<String>
+
+        List<String> names = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "j",
+                        "gender", "Male")
+                .when()
+                .get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .body("totalElement", greaterThanOrEqualTo(3))
+                .extract().response().jsonPath().getList("content.name");
+
+        System.out.println(names);
+
+    }
 
 
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test2() {
 
+        //save status code
+        int statusCode = given().accept(ContentType.JSON)
+                .and().queryParams("nameContains", "j",
+                        "gender", "male")
+                .when().get("/api/spartans/search")
+                .then()
+                .statusCode(200)
+                .body("totalElement", greaterThanOrEqualTo(6))
+                .extract().statusCode();
+
+        System.out.println(statusCode);
+    }
 }
