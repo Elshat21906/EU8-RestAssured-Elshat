98de6bba9276f77981addb056d2615edb915ee22
Elshat <z90602190@hotmail.com>
2022-08-24 14:55:40 +0300

    Day06:Pojo,Post,Put,Patch,Delete

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/Day06_Pojo_Post_Put_Patch_Delete/SpartanPojoGetRequestTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/Day06_Pojo_Post_Put_Patch_Delete/SpartanPojoGetRequestTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.Day06_Pojo_Post_Put_Patch_Delete;
 
+import com.cybertek.pojo.Spartan;
 import com.cybertek.utilitis.SpartansTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -21,6 +22,7 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
                 .when()
                 .get("/api/spartans/{id}")
                 .then().statusCode(200)
+                .log().all()
                 .extract().response();
 
         //De serialize --> JSON to POJO (java custom class)
@@ -28,6 +30,13 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
         //1.using as() method
         //we convert json response to spartan object with the help of jackson
         //as() method uses jackson to de serialize(converting JSON to Java class)
+        Spartan spartan15 = response.as(Spartan.class);
+        System.out.println("spartan15 = " + spartan15);
+
+        System.out.println("spartan15.getId() = " + spartan15.getId());
+        System.out.println("spartan15.getName() = " + spartan15.getName());
+        System.out.println("spartan15.getGender() = " + spartan15.getGender());
+        System.out.println("spartan15.getPhone() = " + spartan15.getPhone());
 
 
         //second way of deserialize json to java
