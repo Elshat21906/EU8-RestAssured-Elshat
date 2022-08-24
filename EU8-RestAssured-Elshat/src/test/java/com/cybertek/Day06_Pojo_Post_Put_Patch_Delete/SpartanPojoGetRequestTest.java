1ae9838170ae9150c21c680de0fc2dacaa32bba2
Elshat <z90602190@hotmail.com>
2022-08-24 17:54:29 +0300

    Day06:Pojo,Post,Put,Patch,Delete

--- EU8-RestAssured-Elshat/src/test/java/com/cybertek/Day06_Pojo_Post_Put_Patch_Delete/SpartanPojoGetRequestTest.java
+++ EU8-RestAssured-Elshat/src/test/java/com/cybertek/Day06_Pojo_Post_Put_Patch_Delete/SpartanPojoGetRequestTest.java
@@ -3,6 +3,7 @@ package com.cybertek.Day06_Pojo_Post_Put_Patch_Delete;
 import com.cybertek.pojo.Spartan;
 import com.cybertek.utilitis.SpartansTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,7 +23,7 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
                 .when()
                 .get("/api/spartans/{id}")
                 .then().statusCode(200)
-                .log().all()
+                .log().body()
                 .extract().response();
 
         //De serialize --> JSON to POJO (java custom class)
@@ -38,9 +39,23 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
         System.out.println("spartan15.getGender() = " + spartan15.getGender());
         System.out.println("spartan15.getPhone() = " + spartan15.getPhone());
 
+        System.out.println("------------------------------------------------------------------");
+
 
         //second way of deserialize json to java
         //2.using JsonPath to deserialize to custom class
+
+        JsonPath jsonPath = response.jsonPath();
+
+        Spartan s15 = jsonPath.getObject("", Spartan.class);
+
+        System.out.println("s15 = " + s15);
+        System.out.println("s15.getId() = " + s15.getId());
+        System.out.println("s15.getName() = " + s15.getName());
+        System.out.println("s15.getGender() = " + s15.getGender());
+        System.out.println("s15.getPhone() = " + s15.getPhone());
+
+
     }
 
 
@@ -50,6 +65,7 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
 
         ///spartans/search?nameContains=a&gender=Male
         // send get request to above endpoint and save first object with type Spartan POJO
+
 
         //get the first spartan from content list and put inside spartan object
 
@@ -69,3 +85,14 @@ public class SpartanPojoGetRequestTest extends SpartansTestBase {
     public void test4(){
     }
 }
+
+
+//De serialize --> JSON to POJO (java custom class)
+//2 different way to do this
+//1.using as() method
+//we convert json response to spartan object with the help of jackson
+//as() method uses jackson to de serialize(converting JSON to Java class)
+
+//second way of deserialize json to java
+//2.using JsonPath to deserialize to custom class
+
